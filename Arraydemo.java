d2bbcfc434629cf83ab627feaf14c7af2df3e3bd
Codefrombasics <codefrombasics@gmail.com>
2024-11-11 09:02:00 +0530

    added loops and Array

---
+++ Arraydemo.java
@@ -0,0 +1,35 @@
+public class Arraydemo {
+    public static void main(String[] args) {
+//         int marks[]=new int[6];
+// marks[0]=65;
+// marks[1]=87;
+// marks[2]=98;
+// marks[3]=86;
+// marks[4] will be filled with default value of int(that is 0)
+// marks[5] will be filled with default value of int(that is 0)
+// System.out.println(marks);
+// for (int mark : marks) {
+//     System.out.println(mark);
+// }
+
+// String myfriends=new String[5];
+String myfriend[]=new String[8];
+for (String friend : myfriend) {
+    System.out.println(friend);
+}
+
+
+int marks[][]={{65,87,96,55,63,72},{64,81,92,53,73,82},{75,97,96,58,63,52}};
+String studentsName[]={"Harikumar","Jagan","Moorthy"};
+
+for (int i = 0; i < marks.length; i++) {
+    System.out.print(studentsName[i]+'-');
+    for (int j = 0; j < marks[i].length; j++) {
+        System.out.print(marks[i][j]+" ");
+    }
+    System.out.println();
+
+}
+
+    }
+}
--- Loops.java
+++ Loops.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
+
 public class Loops {
     public static void main(String[] args) {
         int marks[]={55,66,78,65,72};
         // can go only in forward direction
-        for (int mark : marks) {
-            if(mark==78)
-            continue;
-            System.out.print(mark+" ");
-        }
+        // for (int mark : marks) {
+        //     if(mark==78)
+        //     continue;
+        //     System.out.print(mark+" ");
+        // }
     
         // for (int i = 0; i < marks.length; i++) {
         //     System.out.println(marks[i]);
@@ -27,5 +29,31 @@ public class Loops {
 
         // // }
         // System.out.println("hi");
+
+        // int num1=20;
+        // while (num1<30){
+        //     System.out.println(num1);
+        //     num1++;
+        // }
+
+        //write  a code to get input from user and add all the number he give and ask for his choice, when he says
+        //no stop asking input and print the result
+    //    0+1+2 3 4
+        int total=0;
+        Scanner scanner=new Scanner(System.in);
+        while (true){
+            System.out.println("Enter a number");
+            int num=scanner.nextInt();
+            
+            // System.out.println("would you like to give another number (y/n)");
+            // String choice=scanner.next();//next() will not take the character after space
+            if(num<0){
+                break;
+            }
+            total+=num;
+
+        }
+        System.out.println("total: "+total);
     }
 }
+
