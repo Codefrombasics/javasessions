99b3a80e0c2fc93382b9ac98197172fb58f12529
Codefrombasics <codefrombasics@gmail.com>
2024-11-16 12:31:36 +0530

    added from intellijjhj

--- gettinginputfromuser.java
+++ gettinginputfromuser.java
@@ -4,7 +4,7 @@ public class gettinginputfromuser {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter the input value");
-        int useVal=scanner.nextInt();
-        System.out.println(usrVal+7);
+        int useVal1=scanner.nextInt();
+        System.out.println(useVal1+7);
     }
 }
